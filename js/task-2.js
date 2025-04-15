d8a23bdb15e1a774fc553b57d9aaf1a635d1914c
Vladislav Polishchuk <bayriki@gmail.com>
2025-04-15 11:48:48 +0200

    message

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,7 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    const index = this.#items.inexOf(itmToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
       this.#items.splice(index, 1);
     }
